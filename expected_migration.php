7b2bdca0914162c3a4ed7685ffa4c2b7e055499a
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2015-03-11 14:19:01 +0100

    Initial commit

---
+++ expected_migration.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1426079361.
+ * Generated on 2015-03-11 13:09:21 by kkoenig
+ */
+class PropelMigration_1426079361
+{
+    public $comment = '';
+
+    public function preUp($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp($manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown($manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `propel-issue`
+(
+    `conversion_rate` DECIMAL(10, 3) DEFAULT 1.000 NOT NULL
+) ENGINE=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `propel-issue`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+Propel issue #
+
+When working with a legacy database that has field like the following
+
+```mysql
+  `field_name` decimal(10,3) DEFAULT '1.000' NOT NULL
+```
+
+propel generates a wrong migration omitting the `10,3`
+This can be reproduced using `make test` where a second migration will be created
+because the schema does not match with the generated migration.
+
+
+If you run into trouble on OSX:
+  Unable to open PDO connection [wrapped: SQLSTATE[HY000] [2002] No such file or directory]
+  On Terminal, execute these commands
+
+  sudo mkdir /var/mysql/
+  sudo ln -s /private/tmp/mysql.sock /var/mysql/mysql.sock
+
+
+I think the schema should look like the `expected_schema.xml` to see the differences run `make migrations && diff generated-reversed-database/schema.xml expected_schema.xml`
